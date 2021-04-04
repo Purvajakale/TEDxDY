da636ca25c45236edf7922492cbb8c9523592650
purvaja kale <purvaja2507@gmail.com>
2021-04-05 00:44:05 +0530

    first commit

---
+++ contactus.php
@@ -0,0 +1,409 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>TEDxDYPATILUNIVERSITY</title>
+    <link rel="icon" href="X.png" type="image/png">
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+ <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/js/bootstrap.bundle.min.js" integrity="sha384-ygbV9kiqUc6oa4msXn9868pTtWMgiQaeYH7/t7LECLbyPA2x65Kgf80OJFdroafW" crossorigin="anonymous"></script>
+ <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.5.4/dist/umd/popper.min.js" integrity="sha384-q2kxQ16AaE6UbzuKqyBE9/u/KzioAlnx2maXQHiDX9d4/zp8Ok3f+M7DPm+Ib6IU" crossorigin="anonymous"></script>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/js/bootstrap.min.js" integrity="sha384-pQQkAEnwaBkjpqZ8RU1fF1AKtTcHJwFl3pblpTlHXybJjHpMYo79HY3hIi4NKxyj" crossorigin="anonymous"></script>
+<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
+<script src="https://kit.fontawesome.com/e015eaaae8.js" crossorigin="anonymous"></script>
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+<link href="https://cdnjs.cloudflare.com/ajax/libs/sweetalert/1.1.3/sweetalert.css" rel="stylesheet" />
+<script src="https://cdnjs.cloudflare.com/ajax/libs/sweetalert/1.1.3/sweetalert.min.js"></script>
+<script src="http://code.jquery.com/jquery-3.3.1.min.js"></script>
+ <link rel="stylesheet" href="header.css">
+  <link rel="stylesheet" href="footer.css">
+
+  <script type="text/javascript" src="https://cdn.jsdelivr.net/npm/emailjs-com@2/dist/email.min.js"></script>
+<script type="text/javascript">
+    (function() {
+        emailjs.init("user_e9z13sDfOQiTbCPF5SxpR");
+            })();
+</script>
+
+
+<style type="text/css">
+  body{
+  background-color: black;
+}
+
+  textarea{
+    resize: none;
+  }
+
+ 
+
+  .form-control{
+    background: transparent;
+    border:none;
+    outline: none;
+    border-bottom: 1px solid red;
+  }
+
+  .wrapper{
+  background: black;
+  padding: 30px;
+  box-shadow: 0px 0px 10px rgba(0,0,0,0.1);
+}
+.wrapper .input_field{
+  height: 40px;
+  width: 100%;
+  position: relative;
+}
+.wrapper .input_field input{
+  height: 100%;
+  width: 100%;
+  border: none;
+  outline: none;
+  background-color: black;
+  font-size: 17px;
+  border-bottom: 1px solid red;
+}
+.input_field input:focus ~ label,
+.input_field input:valid ~ label{
+  transform: translateY(-20px);
+  font-size: 15px;
+  color: grey;
+
+}
+.wrapper .input_field label{
+  position: absolute;
+  bottom: 10px;
+  left: 0;
+  color: grey;
+  pointer-events: none;
+  transition: all 0.3s ease;
+}
+.input_field .underline{
+  
+  height: 2px;
+  width: 100%;
+  bottom: 0;
+}
+
+input::-webkit-outer-spin-button,
+input::-webkit-inner-spin-button {
+  -webkit-appearance: none;
+  margin: 0;
+}
+
+#error_message{
+  margin-bottom: 20px;
+  background: #fe8b8e;
+  padding: 0px;
+  text-align: center;
+  font-size: 14px;
+  transition: all 0.5s ease;
+}
+
+ @media only screen and (min-width: 1200px) {
+  iframe{
+         width: 500px; 
+         height: 500px;
+         }
+ }
+
+ @media only screen and (min-width: 1000px) {
+
+      iframe{
+         width: 450px; 
+         height: 450px;
+         }
+
+         .wrapper{
+       width: 450px;
+    }
+  }
+
+
+  @media only screen and (max-width: 600px) {
+    .wrapper{
+       width: 250px;
+    }
+
+  .map{
+    margin-top: 10%;
+  }
+
+  iframe{
+    width: 250px;
+    height: 250px;
+  }
+  }
+
+.recieved{
+  background-color: red;
+  width: 300px;
+  height: 40px;
+  display: none;
+  margin-bottom: 2%;
+  color: white;
+}
+
+
+
+</style>
+
+<header>
+   <nav class="navbar" style="position: sticky;" >
+        <div class="brand-title">
+        	<img src="Images/TEDxDYPatilUniversityop.png" class="responsive logo"></div>
+        <a href="#" class="toggle-button" >
+          <span class="bar"></span>
+          <span class="bar"></span>
+          <span class="bar"></span>
+        </a>
+        <div class="navbar-links" >
+          <ul class="menu" >
+            <li><a class="nav"  href="index.html">HOME</a></li>
+            <li><a class="nav"  href="aboutus.html">ABOUT US</a></li>
+            <li><a class="nav" href="team.html">THE TEAM</a></li>
+            <li><a class="nav" href="speakers.html">SPEAKERS</a></li>
+            <li><a class="nav" href="sponsors.html">SPONSORS</a></li>
+            <li><a class="nav" href="theme.html">THEMES</a></li>
+            <li><a class="nav" href="contactus.php">CONTACT US</a></li>
+          </ul>
+        </div>
+      </nav>
+ </header>
+  </head>
+ <main>
+
+        <center>
+          <div class="container-fluid" style="margin-top: 2%;">
+              <div class="row">
+    <div class="col-md-6 col-lg-6 container-fluid ">
+      <div class="row text-center justify-content-center">
+              <i class="fa fa-envelope" style="font-size:48px;color:red"></i>
+              <br>
+              <div class="container-fluid" >
+              <h4 class="font-weight-30" style="margin-top: 2%;">
+                tedxdypatilu@gmail.com
+              </h4>
+              <p class="margin-8 no-margin-bottom no-margin-rl font-family-alt text-extra-large text-center">
+                Send us your queries by email.<br>
+                We would get back to you within 24 hours.
+              </p>
+            </div></div>
+            <div id="error_message" ></div>
+
+            <!-- <button class="third">Third Alert</button> -->
+
+            <!-- ****************form**************** -->
+      <form  id="myForm" autocomplete="off">
+      <!-- <div class="sent-notification" style="color: white;"></div> -->
+      <div class="container recieved" ><h4 style="font-family: 'Poppins', sans-serif;">Message Recieved</h4></div>
+        <div class="row" >
+       
+
+<!-- full name -->
+      <div class="col" >
+      <div class="wrapper">
+      <div class="input_field">
+        <input id="name" type="text" required>
+        <div class="underline"></div>
+<label>Full Name<span style="color: red;">*</span></label>
+      </div>
+    </div>
+  </div>
+</div>
+
+<!-- number -->
+   <div class="row">
+    <div class="col" >
+      <div class="wrapper">
+      <div class="input_field">
+        <input type="text" id="phone" required>
+        <div class="underline">
+</div>
+<label>Phone Number<span style="color: red;">*</span></label>
+      </div>
+    </div>
+  </div>
+</div>
+
+<!-- mail -->
+ <div class="row" >
+  <div class="col" >
+      <div class="wrapper">
+      <div class="input_field">
+      <input id="email" type="text" required>
+        <div class="underline">
+</div>
+<label>Mail Id<span style="color: red;">*</span></label>
+      </div>
+    </div>
+  </div>
+  </div>
+
+<!-- message -->
+  
+  <div class="row" >
+  <div class="col" >
+      <div class="wrapper">
+      <div class="input_field">
+        <input type="textarea"  rows="4" id="body" required>
+        <div class="underline">
+</div>
+<label>Message<span style="color: red;">*</span></label>
+      </div>
+    </div>
+  </div>
+</div>
+
+
+<button type="submit" class="btn btn-outline-secondary submit-button" id="submit" onclick="sendEmail()" style="margin-top: 2%;background-color: red;color: white;" >Submit</button>
+
+
+</form>
+
+
+
+</div>
+
+ 
+            
+          <div class="col-md-6 col-lg-6 map" >
+            <div class="row text-center justify-content-center">
+              <i class="fa fa-map-marker" style="font-size:48px;color:red"></i>
+              <br>
+              <div class="container-fluid" >
+              <h4 class="font-weight-300 letter-spacing-1" style="margin-top: 2%;">
+                D Y PATIL UNIVERSITY
+              </h4>
+              <p class="margin-8 no-margin-bottom no-margin-rl font-family-alt text-extra-large text-center">
+                 Sector 7, Nerul,Navi Mumbai,<br>Maharashtra 400706.
+              </p>
+            </div>
+           </div>
+            <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d120686.21892512261!2d72.9549502582031!3d19.044191600000005!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3be7c3e74bd776a5%3A0x52553a65b88386d8!2sDY%20Patil%20University!5e0!3m2!1sen!2sin!4v1614688185380!5m2!1sen!2sin" style="border:0;" allowfullscreen="" loading="lazy"></iframe>
+        </div>
+      </div>
+     </div>
+      </center>
+ </main>
+
+ <footer style="margin-top: 2%;" >
+ 	<div class="main-content">
+        <div class="left box">
+          <h2>HAVE QUERIES? ASK US</h2>
+          <div class="content">
+            
+      	<p>Kirti Agarwal (Public Relations) - +91 85528 45537</p>
+      	<p>Afraz Kazi (Partnerships)        - +91 84509 23743</p>
+            
+          </div>
+        </div>
+
+        <div class="center box">
+          <h2>Address</h2>
+          <div class="content">
+            <div class="place">
+              <span class="fas fa-map-marker-alt"></span>
+              <span class="text">D Y Patil University, Navi Mumbai</span>
+            </div>
+            <div class="phone">
+              <span class="fas fa-phone-alt"></span>
+              <span class="text"> IT OFFICE : +91 72768 80013</span>
+            </div>
+            <div class="email">
+              <span class="fas fa-envelope"></span>
+              <span class="text">dypatilu@gmail.com</span>
+            </div>
+          </div>
+        </div>
+
+        <div class="right box">
+          <h2>SOCIAL MEDIA</h2>
+          <div class="content">
+            <div class="social">
+              <a href="https://www.facebook.com/268666783613875/"><span class="fab fa-facebook-f"></span></a>
+              <a href="https://twitter.com/TEDxDYPatil?s=08"><span class="fab fa-twitter"></span></a>
+              <a href="https://instagram.com/tedxdypatiluniversity?igshid=13hkv6yb1uaak"><span class="fab fa-instagram"></span></a>
+              <a href="https://www.youtube.com/results?search_query=tedx+dy+patil+university"><span class="fab fa-youtube"></span></a>
+              <a href="https://www.linkedin.com/company/tedxdypatiluniversity"><span class="fab fa-linkedin-in"></span></a>
+              <a href="https://discord.gg/7ZzYdpah"><span class="fab fa-discord" ></span></a>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div class="bottom">
+        <center>
+          <span class="credit">This independent TEDx event is operated under a license from TED. | </span>
+          <span class="far fa-copyright"></span><span> 2021 All rights reserved.</span>
+        </center>
+      </div>
+ </footer>
+
+ 
+ 
+
+ 	<script type="text/javascript">
+
+	$(document).ready(function(){
+  $(".toggle-button").click(function(){
+    $(".navbar-links").slideToggle("slow");
+  });
+});
+
+
+
+function sendEmail() {
+            var name = $("#name");
+            var email = $("#email");
+            var phone = $("#phone");
+            var body = $("#body");
+
+            if (isNotEmpty(name) && isNotEmpty(email) && isNotEmpty(phone) && isNotEmpty(body)) {
+                $.ajax({
+                   url: 'sendEmail.php',
+                   method: 'POST',
+                   dataType: 'json',
+                   data: {
+                       name: name.val(),
+                       email: email.val(),
+                       phone: phone.val(),
+                       body: body.val()
+                   }, success: function (response) {
+                       
+                        alert('hello')
+                   }
+                });
+            }
+        }
+
+        function isNotEmpty(caller) {
+            if (caller.val() == "") {
+                return false;
+            } else
+            return true;
+        }
+
+//         function validateForm() {
+//   var x = document.forms["#myForm"]["#name"].value;
+//   if (x == "") {
+    
+//   }
+//   else{
+//     $(document).ready(function(){
+//   $(".submit-button").click(function(){
+//     $(".recieved").slideToggle("slow");
+//   });
+// });
+//   }
+// }
+
+// $(document).ready(function(){
+//   $(".submit-button").click(function(){
+//     $(".recieved").slideToggle("slow");
+//   });
+// });
+
+       
+
+ </script>
